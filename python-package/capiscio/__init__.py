5c4a8a2a593d8029d42174d5dddee9ae0915e0f5
Beon de Nood <beon@bigdune.net>
2025-10-14 14:39:46 -0400

    chore: update branding from Capiscio to CapiscIO
    - Update copyright in LICENSE
    - Update author field in package.json
    - Update Python package docstrings and metadata
    - Consistent capitalization: CapiscIO (capital I and O)

--- python-package/capiscio/__init__.py
+++ python-package/capiscio/__init__.py
@@ -1,10 +1,10 @@
 """
-Capiscio - A2A Protocol Validator and CLI Tool
+CapiscIO - A2A Protocol Validator and CLI Tool
 
-This package provides a Python wrapper around the Capiscio CLI tool,
+This package provides a Python wrapper around the CapiscIO CLI tool,
 allowing easy installation and usage via pip.
 """
 
 __version__ = "2.0.0"
-__author__ = "Capiscio Team"
+__author__ = "CapiscIO Team"
 __email__ = "hello@capiscio.com"
